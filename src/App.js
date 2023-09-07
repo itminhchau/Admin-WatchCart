e854a22f05590fee3cc99fb129a5d3b08381a786
itminhchau <daominhchau18@gmail.com>
2023-09-07 17:08:55 +0700

    [add] axios react-router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import Home from 'features/Home';
+import { Route, Routes } from 'react-router-dom';
+
 function App() {
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold underline text-red-400">
-        Hello world!
-      </h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   );
 }
